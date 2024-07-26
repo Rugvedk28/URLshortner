2fddd7e87a799881cb00490461f4624880100bc8
Rugved Kulkarni <rugvedrk1189@gmail.com>
2024-07-26 21:32:33 +0530

    nm

--- routes/url.js
+++ routes/url.js
@@ -1,6 +1,13 @@
 const express = require("express");
-const {handleGenerateNewShortURL} = require("../controller/url");
+const {
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
+} = require("../controllers/url");
+
 const router = express.Router();
 
 router.post("/", handleGenerateNewShortURL);
+
+router.get("/analytics/:shortId", handleGetAnalytics);
+
 module.exports = router;
